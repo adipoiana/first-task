5533c8597f9024578b4a1ef6a73aa1ceb0e1e280
adipoiana <ady_poiana@hotmail.com>
2023-08-01 15:46:38 +0300

    second commit

---
+++ extern/style_autentificare.php
@@ -0,0 +1,81 @@
+@import url('https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans:wght@500&display=swap');
+body {
+	font-family: 'Lato';
+}
+.header{
+	height: 200px;
+	background-color: #606060 ;
+}
+.container {
+	width: 800px !important;
+	height: 200px;
+	margin: 0 auto;
+
+}
+.logo{
+	width: 350px;
+	height: 100px;
+	margin: 0 auto ;
+	padding-top: 25px;
+
+	
+
+}
+.logo img {
+	width: 100%;
+	
+}
+.nav {
+	margin-top: 50px;
+
+}
+.nav ul{
+	margin: 0 auto;
+	font-size: 23px;
+	font-family: 'Lato';
+	
+}
+.nav ul li{
+	display: inline-block;
+	padding: 0 25px;
+
+}
+a {
+	text-decoration: none !important;
+	color: black !important;
+}
+.login {
+	margin-top: 150px;
+}
+.login h1{
+text-align: center;
+font-size: 40px;
+font-family: lato;
+margin-bottom: 25px;
+}
+.login form{
+	text-align: center;
+}
+.login form input{
+	width: 300px;
+	height: 35px;
+	margin-bottom: 20px;
+	text-indent: 5px;
+	padding: 2.5px;
+	
+
+}
+.login form input:last-child{
+	background-color: #25a4f4;
+	border: none;
+	width: 310px;
+	color: white;
+	height: 40px;
+	cursor: pointer;
+	transition: 0.5s;
+}
+.login form input:last-child:hover{
+	background-color: #f63232;
+}
+
+
